ea0f33618fde129e11d68fc051741a81bfc4a0bb
Jesse Chua <13chuajesse@gmail.com>
2018-06-01 15:38:49 -0700

    Add mysql "shop_reviews" connection to server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mysql = require("mysql");
 
 const app = express();
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "shop_reviews"
+});
+
+connection.connect();
 
 app.listen(3001);
 console.log("Express server listening on port 3001");
