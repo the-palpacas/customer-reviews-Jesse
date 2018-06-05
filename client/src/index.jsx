f6ed4ee2b346fc8fdeae78cc40ea7a5476b211e9
Jesse Chua <13chuajesse@gmail.com>
2018-06-05 11:52:50 -0700

    Create basic App component.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,14 @@
 import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Hello World</div>
+    )
+  }
+}
