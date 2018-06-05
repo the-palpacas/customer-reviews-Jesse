e9748c6553f5d55a5cf2f0a985d16649200e5337
Jesse Chua <13chuajesse@gmail.com>
2018-06-05 12:00:03 -0700

    Add axios module. Add state for reviews. Implement componentDidMount GET request to server for review objects

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      reviews: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`${window.location.pathname}reviews`)
+      .then(response => {
+        this.setState({reviews: response});
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
