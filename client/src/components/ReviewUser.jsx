b34aa8d9fbd0c5374b2fbf9993dfc1423dfd100f
Jesse Chua <13chuajesse@gmail.com>
2018-06-06 12:00:49 -0700

    Create ReviewUser component.

---
+++ client/src/components/ReviewUser.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ReviewUser = (props) => {
+  return (
+    <div>
+      <div>Picture goes here</div>
+      <div>Reviewed by (Username goes here)</div>
+    </div>
+  )
+}
+
+export default ReviewUser;
