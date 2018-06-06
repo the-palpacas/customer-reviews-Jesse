82ec0ef0fddb1ad14da8cc6480b1473a4db3e949
Jesse Chua <13chuajesse@gmail.com>
2018-06-06 12:24:41 -0700

    Add imports for ReviewEntry and SubmitReview to App component. Add ReviewEntry and SubmitReview to render function.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
+import ReviewEntry from "./components/ReviewEntry.jsx";
+import SubmitReview from "./components/SubmitReview.jsx";
 
 class App extends React.Component {
   constructor(props) {
@@ -21,7 +23,14 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>Hello World</div>
+      <div>
+        {this.state.reviews.map(review => {
+          return (
+            <ReviewEntry />
+          )
+        })}
+        <SubmitReview />
+      </div>
     )
   }
 }
