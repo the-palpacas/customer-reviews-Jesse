0f5b7ec11e7fba73c4e48dc4a63f026c7b263073
Jesse Chua <13chuajesse@gmail.com>
2018-06-06 12:09:16 -0700

    Create ReviewBody component.

---
+++ client/src/components/ReviewBody.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ReviewBody = (props) => {
+  return (
+    <div>
+      <div><span>Stars go here</span><span>Date goes here</span></div>
+      <div>Review goes here</div>
+      <div>Product image and product name go here</div>
+    </div>
+  )
+}
+
+export default ReviewBody;
