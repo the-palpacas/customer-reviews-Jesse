4fbe73c788e1d76c77423cbed88ad52497af29a9
Jesse Chua <13chuajesse@gmail.com>
2018-06-07 16:31:01 -0700

    Create Helpfulness component.

---
+++ client/src/components/Helpfulness.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Helpfulness = (props) => {
+  return (
+    <div>
+      <div>(Helpfulness prop) out of (Votes prop) people found this review helpful.</div>
+      <button>Helpful</button><button>Not Helpful</button>
+    </div>
+  )
+}
+
+export default Helpfulness;
