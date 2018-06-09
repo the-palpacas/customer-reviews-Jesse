1b660201015f3ebe24c5539ca8b04a1a0f396177
Jesse Chua <13chuajesse@gmail.com>
2018-06-09 02:15:48 -0700

    Reformat date to be in "Month Day, Year" format.

--- client/src/components/ReviewBody.jsx
+++ client/src/components/ReviewBody.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
 const ReviewBody = (props) => {
+  let months = {
+    "01": "Jan", 
+    "02": "Feb", 
+    "03": "Mar", 
+    "04": "Apr",
+    "05": "May", 
+    "06": "Jun", 
+    "07": "Jul", 
+    "08": "Aug",
+    "09": "Sept", 
+    "10": "Oct", 
+    "11": "Nov", 
+    "12": "Dec"
+  };
+
+  let [month, day, year] = props.date.split("/");
+  let formatted = `${months[month]} ${day}, ${year}`;  
+
   return (
     <div>
-      <div><span>{props.rating} stars</span> <span>{props.date}</span></div>
+      <div><span>{props.rating} stars</span> <span>{formatted}</span></div>
       <div>{props.review}</div>
       <a href={`/${props.productId}/`}><img src={props.img}/>{props.productName}</a>
     </div>
