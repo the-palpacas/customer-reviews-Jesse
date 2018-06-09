e47d758ed38fea6bfa1bd9559935709b6537e0a6
Jesse Chua <13chuajesse@gmail.com>
2018-06-08 22:04:21 -0700

    Change Helpfulness component to be stateful.

--- client/src/components/Helpfulness.jsx
+++ client/src/components/Helpfulness.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 
-const Helpfulness = (props) => {
-  return (
-    <div>
-      <div><i className="fas fa-arrow-up" /></div>
-      <div>{props.helpfulness}</div>
-      <div><i className="fas fa-arrow-down" /></div>
-    </div>
-  )
+class Helpfulness extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected: null
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div><i className="fas fa-arrow-up" /></div>
+        <div>{props.helpfulness}</div>
+        <div><i className="fas fa-arrow-down" /></div>
+      </div>
+    )
+  }
 }
 
 export default Helpfulness;
