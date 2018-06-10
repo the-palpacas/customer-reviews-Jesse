65038e0a423649f7adecee9e6a507fd0652505e1
Jesse Chua <13chuajesse@gmail.com>
2018-06-09 20:22:58 -0700

    Import styled-components and resize image.

--- client/src/components/ReviewUser.jsx
+++ client/src/components/ReviewUser.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 
 const ReviewUser = (props) => {
+  const RoundedImg = styled.img`
+    height: 50px;
+    width: 50px;
+    border-radius: 25px;
+  `;
+  
   return (
     <div>
-      <img src={props.img} />
+      <RoundedImg src={props.img} />
       <div>Reviewed by {props.username}</div>
     </div>
   )
